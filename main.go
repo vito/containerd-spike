f6c43dfb98b7a914a3a540fcea97ed6bcbfc377e
Alex Suraci <suraci.alex@gmail.com>
2019-09-02 00:37:46 -0400

    do garden-style uid/gid mapping
    incrementing the uid/gid of every single file in the system is
    unnecessary. just remap root to (max id) and map 1+ to 1-(max id - 1)
    instead; then we only have to chown the root-owned files.
    
    this somehow, some way, fixes building from Busybox.
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
+	"syscall"
 
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/cio"
+	"github.com/containerd/containerd/containers"
+	"github.com/containerd/containerd/defaults"
+	"github.com/containerd/containerd/errdefs"
+	"github.com/containerd/containerd/mount"
 	"github.com/containerd/containerd/namespaces"
 	"github.com/containerd/containerd/oci"
 	"github.com/containerd/containerd/runtime/linux/runctypes"
 	"github.com/containerd/containerd/runtime/v2/runc/options"
 	"github.com/containerd/go-cni"
+	"github.com/opencontainers/image-spec/identity"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -28,6 +35,9 @@ func main() {
 }
 
 func run() error {
+	maxUid := uint32(MustGetMaxValidUID())
+	maxGid := uint32(MustGetMaxValidUID())
+
 	client, err := containerd.New("/run/containerd/containerd.sock")
 	if err != nil {
 		return err
@@ -38,6 +48,7 @@ func run() error {
 
 	// image, err := importImage(ctx, client, "oci-build-task.tar")
 	image, err := client.Pull(ctx, "docker.io/vito/oci-build-task:latest", containerd.WithPullUnpack)
+	// image, err := client.Pull(ctx, "docker.io/vito/containerd-test:latest", containerd.WithPullUnpack)
 	if err != nil {
 		return err
 	}
@@ -77,7 +88,7 @@ func run() error {
 	container, err := client.NewContainer(
 		ctx,
 		"some-container",
-		containerd.WithRemappedSnapshot("some-container-snapshot", image, 1000, 1000),
+		withRemappedSnapshotBase("some-container-snapshot", image, maxUid, maxGid, false),
 		// containerd.WithNewSnapshot("some-container-snapshot", image),
 		containerd.WithNewSpec(
 			oci.WithImageConfig(image),
@@ -92,8 +103,10 @@ func run() error {
 				"CAP_NET_ADMIN",
 			}),
 
-			// enable user namespace (i.e. unprivileged container)
-			oci.WithUserNamespace(0, 1000, 10000),
+			// enable user namespaces
+			// XXX: there's no way to specifically use maxGid here :/
+			oci.WithUserNamespace(0, maxUid, 1),
+			oci.WithUserNamespace(1, 1, maxUid-1),
 
 			// ...just set a hostname
 			oci.WithHostname("concourse"),
@@ -101,6 +114,8 @@ func run() error {
 			// wire up concourse stuff
 			oci.WithMounts(mounts),
 			oci.WithProcessCwd("/inputs"),
+			// oci.WithUIDGID(0, 0), // disable rootless for now
+			// oci.WithProcessArgs("task"),
 		),
 	)
 	if err != nil {
@@ -127,13 +142,13 @@ func run() error {
 		container.Info(ctx)
 		if containerd.CheckRuntime(info.Runtime.Name, "io.containerd.runc") {
 			copts = &options.Options{
-				IoUid: 1000,
-				IoGid: 1000,
+				IoUid: maxUid,
+				IoGid: maxGid,
 			}
 		} else {
 			copts = &runctypes.CreateOptions{
-				IoUid: 1000,
-				IoGid: 1000,
+				IoUid: maxUid,
+				IoGid: maxGid,
 			}
 		}
 
@@ -154,6 +169,9 @@ func run() error {
 	}
 
 	netns := fmt.Sprintf("/proc/%d/ns/net", task.Pid())
+
+	// XXX: work around lack of reliable cleanup during dev
+	_ = network.Remove(ctx, "some-network", "")
 
 	_, err = network.Setup(ctx, "some-network", netns)
 	if err != nil {
@@ -191,6 +209,114 @@ func run() error {
 	return nil
 }
 
+func withRemappedSnapshotBase(id string, i containerd.Image, uid, gid uint32, readonly bool) containerd.NewContainerOpts {
+	return func(ctx context.Context, client *containerd.Client, c *containers.Container) error {
+		diffIDs, err := i.RootFS(ctx)
+		if err != nil {
+			return err
+		}
+
+		var (
+			parent   = identity.ChainID(diffIDs).String()
+			usernsID = fmt.Sprintf("%s-%d-%d", parent, uid, gid)
+		)
+
+		c.Snapshotter, err = resolveSnapshotterName(client, ctx, c.Snapshotter)
+		if err != nil {
+			return err
+		}
+
+		snapshotter := client.SnapshotService(c.Snapshotter)
+
+		if _, err := snapshotter.Stat(ctx, usernsID); err == nil {
+			if _, err := snapshotter.Prepare(ctx, id, usernsID); err == nil {
+				c.SnapshotKey = id
+				c.Image = i.Name()
+				return nil
+			} else if !errdefs.IsNotFound(err) {
+				return err
+			}
+		}
+
+		mounts, err := snapshotter.Prepare(ctx, usernsID+"-remap", parent)
+		if err != nil {
+			return err
+		}
+		if err := remapRootFS(ctx, mounts, uid, gid); err != nil {
+			snapshotter.Remove(ctx, usernsID)
+			return err
+		}
+		if err := snapshotter.Commit(ctx, usernsID, usernsID+"-remap"); err != nil {
+			return err
+		}
+		if readonly {
+			_, err = snapshotter.View(ctx, id, usernsID)
+		} else {
+			_, err = snapshotter.Prepare(ctx, id, usernsID)
+		}
+		if err != nil {
+			return err
+		}
+		c.SnapshotKey = id
+		c.Image = i.Name()
+		return nil
+	}
+}
+
+func resolveSnapshotterName(c *containerd.Client, ctx context.Context, name string) (string, error) {
+	if name == "" {
+		label, err := c.GetLabel(ctx, defaults.DefaultSnapshotterNSLabel)
+		if err != nil {
+			return "", err
+		}
+
+		if label != "" {
+			name = label
+		} else {
+			name = containerd.DefaultSnapshotter
+		}
+	}
+
+	return name, nil
+}
+
+func remapRootFS(ctx context.Context, mounts []mount.Mount, uid, gid uint32) error {
+	return mount.WithTempMount(ctx, mounts, func(root string) error {
+		return filepath.Walk(root, remapRoot(root, uid, gid))
+	})
+}
+
+func remapRoot(root string, toUid, toGid uint32) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		stat := info.Sys().(*syscall.Stat_t)
+
+		var remap bool
+
+		uid := stat.Uid
+		if uid == 0 {
+			remap = true
+			uid = toUid
+		}
+
+		gid := stat.Gid
+		if gid == 0 {
+			remap = true
+			gid = toGid
+		}
+
+		if !remap {
+			return nil
+		}
+
+		// be sure the lchown the path as to not de-reference the symlink to a host file
+		return os.Lchown(path, int(uid), int(gid))
+	}
+}
+
 func importImage(ctx context.Context, client *containerd.Client, path string) (containerd.Image, error) {
 	imageFile, err := os.Open(path)
 	if err != nil {
@@ -223,4 +349,88 @@ func importImage(ctx context.Context, client *containerd.Client, path string) (c
 	}
 
 	return image, nil
+}
+
+type IDMap string
+
+const DefaultUIDMap IDMap = "/proc/self/uid_map"
+const DefaultGIDMap IDMap = "/proc/self/gid_map"
+
+const maxInt = int(^uint(0) >> 1)
+
+func MustGetMaxValidUID() int {
+	return must(DefaultUIDMap.MaxValid())
+}
+
+func MustGetMaxValidGID() int {
+	return must(DefaultGIDMap.MaxValid())
+}
+
+func (u IDMap) MaxValid() (int, error) {
+	f, err := os.Open(string(u))
+	if err != nil {
+		return 0, err
+	}
+
+	var m uint
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		var container, host, size uint
+		if _, err := fmt.Sscanf(scanner.Text(), "%d %d %d", &container, &host, &size); err != nil {
+			return 0, ParseError{Line: scanner.Text(), Err: err}
+		}
+
+		m = minUint(maxUint(m, container+size-1), uint(maxInt))
+	}
+
+	return int(m), nil
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func maxUint(a, b uint) uint {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func minUint(a, b uint) uint {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+type ParseError struct {
+	Line string
+	Err  error
+}
+
+func (p ParseError) Error() string {
+	return fmt.Sprintf(`%s while parsing line "%s"`, p.Err, p.Line)
+}
+
+func must(a int, err error) int {
+	if err != nil {
+		panic(err)
+	}
+
+	return a
 }
